60bad84655db2cfad6949c33cd52396bbc83cd02
jagan <rockjagan.822@gmail.com>
2022-06-25 15:42:08 +0530

    Create Action_Types.js

---
+++ Redux/Action/Action_Types.js
@@ -0,0 +1,14 @@
+/*
+All types of action are declared here for common reference name
+export const Var_name="some descriptions or same Var_name"
+*/
+
+// Common Actions
+export const Login = 'Login Successfull';
+export const SignUp = 'New User Creation';
+export const Token = 'auth token';
+// Core Actions
+
+// Networks Actions
+
+// Others Actions
